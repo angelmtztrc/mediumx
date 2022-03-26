df9e4ac5752b2976adcc35e0969cb91bbad0ce55
Angel Martinez <angel.martineztrc@gmail.com>
2022-03-26 16:41:15 -0500

    feat:  crud for categories

--- api/src/app/categories/categories.controller.ts
+++ api/src/app/categories/categories.controller.ts
@@ -1,7 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+
+import { Category } from './entities/category.entity';
+
 import { CategoriesService } from './categories.service';
+
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
 
 @Controller('categories')
 export class CategoriesController {
-  constructor(private readonly categoriesService: CategoriesService) {}
+  constructor(private readonly service: CategoriesService) {}
+
+  @Post()
+  create(@Body() input: CreateCategoryDto): Promise<Category> {
+    return this.service.create(input);
+  }
+
+  @Get()
+  getAll(): Promise<Category[]> {
+    return this.service.findAll();
+  }
+
+  @Get(':id')
+  getOneById(@Param('id') id: number): Promise<Category> {
+    return this.service.findOneById(id);
+  }
+
+  @Put(':id')
+  update(@Param('id') id: number, @Body() input: UpdateCategoryDto): Promise<Category> {
+    return this.service.update(id, input);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: number): Promise<string> {
+    return this.service.remove(id);
+  }
 }
--- api/src/app/categories/categories.service.ts
+++ api/src/app/categories/categories.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { CreateCategoryDto } from './dto/create-category.dto';
+import { UpdateCategoryDto } from './dto/update-category.dto';
+
+import { Category } from './entities/category.entity';
 
 @Injectable()
-export class CategoriesService {}
+export class CategoriesService {
+  constructor(@InjectRepository(Category) private readonly repository: Repository<Category>) {}
+
+  async create(input: CreateCategoryDto): Promise<Category> {
+    input.name = input.name.toLowerCase();
+
+    const isExisting = await this.repository
+      .createQueryBuilder('category')
+      .where('category.name = :name', { name: input.name })
+      .getOne();
+    if (isExisting) throw new BadRequestException('Category already exists.');
+
+    const category = this.repository.create(input);
+    return await this.repository.save(category);
+  }
+
+  async findAll(): Promise<Category[]> {
+    return await this.repository.createQueryBuilder('category').getMany();
+  }
+
+  async findOneById(id: number): Promise<Category> {
+    return await this.repository
+      .createQueryBuilder('category')
+      .where('category.id = :id', { id })
+      .getOne();
+  }
+
+  async update(id: number, input: UpdateCategoryDto) {
+    input.name = input.name.toLowerCase();
+
+    const category = await this.findOneById(id);
+    if (!category) throw new NotFoundException('Category not found.');
+
+    return await this.repository.save({ ...category, ...input });
+  }
+
+  async remove(id: number) {
+    const category = await this.findOneById(id);
+    if (!category) throw new NotFoundException('Category not found.');
+
+    await this.repository.remove(category);
+
+    return 'Category deleted successfully.';
+  }
+}
